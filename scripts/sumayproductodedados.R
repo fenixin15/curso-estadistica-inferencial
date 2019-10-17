01194c69b61cd9dedbace777de27d5d0fe95c69c
x1r <r.alberich@uib.es>
2019-10-17 11:51:06 +0200

    spcrip dados

--- scripts/sumayproductodedados.R
+++ scripts/sumayproductodedados.R
@@ -6,9 +6,11 @@ pdado(1.5,n=6)
 pdado(1:10,n=6)
 
 
-curve(pdado,xlim=c(1,6))
+curve(pdado,xlim=c(1,6),type="h",col="blue")
 pdado2 =function(x,y,n=6) {pdado(x,n)*pdado(y,n)}
-
+pdado2(6,5,n=6)
+1/36
+pdado2(6,7,n=6)
 
 psumadados=function(x,n=6) {
   Dxy=data.frame(d1=rep(1:n,each=n),d2=rep(1:n,times=n))
@@ -19,6 +21,8 @@ psumadados=function(x,n=6) {
 
 psumadados(7,10)
 
+psumadados(7,6)
+
 pproddados=function(x,n=6) {
     Dxy=data.frame(d1=rep(1:n,each=n),d2=rep(1:n,times=n))
     Dxy$producto=Dxy$d1*Dxy$d2
@@ -28,4 +32,24 @@ pproddados=function(x,n=6) {
 
 pproddados(13)
 
+psum_prod=function(x,y,n=6){
+  Dxy=data.frame(d1=rep(1:n,each=n),d2=rep(1:n,times=n))
+  Dxy$suma=Dxy$d1+Dxy$d2
+  Dxy$producto=Dxy$d1*Dxy$d2
+  aux=Dxy[Dxy$suma==x& Dxy$producto==y,]
+  sum(apply(aux[,1:2],FUN=function(x) {pdado2(x[1],x[2],n=n)},1 ))
+}
 
+
+psum_prod(6,5,n=6)
+n=6
+
+Dxy=data.frame(d1=rep(1:n,each=n),d2=rep(1:n,times=n))
+Dxy$suma=Dxy$d1+Dxy$d2
+Dxy$producto=Dxy$d1*Dxy$d2
+Dxy
+sapply()
+
+round(prop.table(table(Dxy$suma,Dxy$producto)),4)
+
+knitr::kable(round(prop.table(table(Dxy$suma,Dxy$producto)),4))
